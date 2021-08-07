71804af56b91a8c590481f51001eecd252119a37
farrukh90 <farrukhsadykov@gmail.com>
2021-08-07 19:41:07 +0000

    added provider

--- Google/provider.tf
+++ Google/provider.tf
@@ -1,7 +1,8 @@
 data "google_billing_account" "acct" {
-	display_name = "terraform_class"
+	display_name = "terraform_google_class"
 	open = true
 }
+
 resource "random_password" "password" {
 	length = 16
 	number = false
@@ -9,8 +10,22 @@ resource "random_password" "password" {
 	lower = true
 	upper = false
 }
-resource "google_project" "testproject" {
+
+resource "google_project" "terraform" {
 	name = "testproject"
 	project_id = random_password.password.result
 	billing_account = data.google_billing_account.acct.id
+}
+
+resource "null_resource" "set-project" {
+	provisioner "local-exec" {
+	command = "gcloud config set project ${google_project.terraform.project_id}"
+	}
+}
+
+resource "null_resource" "unset-project" {
+	provisioner "local-exec" {
+	when = destroy
+	command = "gcloud config unset project"
+	}
 }
