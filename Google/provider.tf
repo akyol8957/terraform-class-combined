b1547d22c2255d5fac2198f14951408afaec4b9f
farrukh90 <farrukhsadykov@gmail.com>
2021-08-07 13:38:42 +0000

    added google

---
+++ Google/provider.tf
@@ -0,0 +1,16 @@
+data "google_billing_account" "acct" {
+	display_name = "terraform_class"
+	open = true
+}
+resource "random_password" "password" {
+	length = 16
+	number = false
+	special = false
+	lower = true
+	upper = false
+}
+resource "google_project" "testproject" {
+	name = "testproject"
+	project_id = random_password.password.result
+	billing_account = data.google_billing_account.acct.id
+}
